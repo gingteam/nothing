504b5faf65adfb091c8245a5fb8afe981dac2b10
ging-dev <gingdz11012001@gmail.com>
2021-07-07 20:40:20 +0700

    Update

--- app/Helper/Ulogin.php
+++ app/Helper/Ulogin.php
@@ -4,6 +4,39 @@ declare(strict_types=1);
 
 namespace App\Helper;
 
+use InvalidArgumentException;
+
 class Ulogin
 {
+    protected $data;
+
+    public function __construct(string $token, string $host)
+    {
+        $request = file_get_contents('http://ulogin.ru/token.php?token='.$token.'&host='.$host);
+        $response = json_decode($request);
+
+        $this->data = $response;
+
+        if (isset($response->error)) {
+            throw new InvalidArgumentException('Invalid input parameter');
+        }
+    }
+
+    public function getName()
+    {
+        return sprintf('%s %s',
+            $this->data->first_name,
+            $this->data->last_name
+        );
+    }
+
+    public function getId()
+    {
+        return $this->data->uid;
+    }
+
+    public function getPhoto()
+    {
+        return $this->data->photo_big;
+    }
 }
--- app/Presenters/SignPrensenter.php
+++ app/Presenters/SignPrensenter.php
@@ -4,35 +4,35 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Helper\Ulogin;
 use App\Model\User;
+use InvalidArgumentException;
 
 final class SignPresenter extends BasePresenter
 {
     public function actionIn()
     {
         $request = $this->getHttpRequest();
+
         if ($token = $request->getPost('token')) {
-            $s = file_get_contents('http://ulogin.ru/token.php?token='.$token.'&host='.$this->link('this'));
-            $user = json_decode($s);
+            try {
+                $ulogin = new Ulogin($token, $this->link('this'));
 
-            if (isset($user->error)) {
-                $this->flashMessage('Login failed', 'error');
-                $this->redirect('Homepage:');
+                $row = User::query()->firstOrCreate(
+                    ['facebook_id' => $ulogin->getId()],
+                    [
+                        'name' => $ulogin->getName(),
+                        'photo' => $ulogin->getPhoto(),
+                        'remember_token' => $token,
+                    ]
+                );
 
-                return;
+                $this->getUser()->login((string) $row->id);
+                $this->flashMessage('Login successful', 'info');
+            } catch (InvalidArgumentException $e) {
+                $this->flashMessage($e->getMessage(), 'danger');
             }
 
-            $row = User::query()->firstOrCreate(
-                ['facebook_id' => $user->uid],
-                [
-                    'name' => $user->first_name.' '.$user->last_name,
-                    'photo' => $user->photo_big,
-                    'remember_token' => $token,
-                ]
-            );
-
-            $this->getUser()->login((string) $row->id);
-            $this->flashMessage('Login successful', 'info');
             $this->redirect('Homepage:');
         }
     }
